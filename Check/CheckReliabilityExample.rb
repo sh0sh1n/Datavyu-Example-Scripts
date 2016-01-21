1e430da40222cc68db21149478b0e5198fc53d06
Shohan Hasan <shohan.hasan@nyu.edu>
2016-01-20 19:44:43 -0500

    Move scripts to folder based on function. Update script to check reliability.

--- Check/CheckReliabilityExample.rb
+++ Check/CheckReliabilityExample.rb
@@ -1,3 +1,13 @@
+# Checks reliability on columns trial and rel_trial using code "trialnum" as matching key.
+
+## Params
+primary_column_name = 'trial'
+reliability_column_name = 'rel_trial'
+matching_code = 'trialnum'
+leniency_ms = 100
+output_file = '~/Desktop/Relcheck.txt'
+
+## Methods
 require 'Datavyu_API.rb'
 
 def getCellFromTime(col, time)
@@ -20,12 +30,14 @@ def printCellArgs(cell)
   return s
 end
 
+## Body
 begin
-    #$debug=true
+  # Convert relative path and symbols to fully qualified path
+  output_file = File.expand_path(output_file)
 
     # Check argument example
     # Format: "primary column", "reliability column", "variable that is the same in each cell (like a trial number)", the difference between primary times and rel times that is OK, output file (use "") for no output file
     # Time check is in milliseconds
-    check_rel("trial", "rel.trial", "trialnum", 100, "/Users/motoruser/Desktop/Relcheck.txt")
+    check_rel(primary_column_name, reliability_column_name, matching_code, leniency_ms, output_file)
 
 end
