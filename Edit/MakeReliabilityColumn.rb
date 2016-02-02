3d4ca10d6f897fb8c58e189db990836c82989d0e
Shohan Hasan <shohan.hasan@nyu.edu>
2016-02-01 23:54:19 -0500

    Remove unnecessary code. Replace period in new column name with an underscore.

--- Edit/MakeReliabilityColumn.rb
+++ Edit/MakeReliabilityColumn.rb
@@ -1,31 +1,9 @@
 require 'Datavyu_API.rb'
 
-def getCellFromTime(col, time)
-  for cell in col.cells
-    if cell.onset <= time and cell.offset >= time
-      return cell
-    end
-  end
-  return nil
-end
-
-def printCellArgs(cell)
-  s = Array.new
-  s << cell.ordinal.to_s
-  s << cell.onset.to_s
-  s << cell.offset.to_s
-  for arg in cell.arglist
-    s << cell.get_arg(arg)
-  end
-  return s
-end
-
 begin
-    #$debug=true
 
     # Make rel example
     # Format: "rel column name", "variable to make rel from", "multiple to keep (2 is every other cell)", "carry over argument1", "carry over argument2", ...
-    make_rel("rel.trial", "trial", 2, "onset", "offset", "trialnum")
-
+    make_rel("rel_trial", "trial", 2, "onset", "offset", "trialnum")
 
 end
