8572fd4b571dac04038ab8efc3e2374a16ce41a6
Shohan Hasan <shohan.hasan@nyu.edu>
2016-03-08 15:22:19 -0500

    Finish import script.

--- Import/ImportData.rb
+++ Import/ImportData.rb
@@ -3,27 +3,35 @@
 ## Params
 input_file = '~/Desktop/data.csv'
 delimiter = ','
-start_row = 2 # Row to start reading data from; first line is row 1
+start_row = 1 # Row to start reading data from; first line is row 1
 
 # Denote how columns from the input file will be represented in the datavyu spreadsheet
 # This is a nested associative array.
 # The outer key is the name of column.
 # The inner keys are names of codes, and the values for the inner keys are the indices of input
 # columns containing the values for the code. The first column of the input is column 1.
-code_map{
-  'id' => {
-    'study' => 5, # id data starts at column 5
-    'trialdate' => 6,
-    'subjectnum' => 7,
-    'birthdate' => 8,
-    'gender' => 9
+code_map = {
+  'id' => { # id data starts at column 5
+    'onset' => 5,
+    'offset' => 6,
+    'study' => 7,
+    'trialdate' => 8,
+    'subjectnum' => 9,
+    'birthdate' => 10,
+    'gender' => 11
   },
-  'condition' => {
-    'cond' => 12 # condition data is in column 12
+  'condition' => { # condition data is in column 12
+    'ordinal' => 12,
+    'onset' => 13,
+    'offset' => 14,
+    'cond' => 15
   },
-  'trial' => {
-    'trialnum' => 15, # trial data starts at column 16
-    'outcome' => 16
+  'trial' => {  # trial data starts at column 16
+    'ordinal' => 16,
+    'onset' => 17,
+    'offset' => 18,
+    'trialnum' => 19,
+    'outcome' => 20
   }
 }
 
@@ -37,7 +45,7 @@ begin
   columns = {}
   code_map.each_pair do |column_name, pairs|
     codes = pairs.keys
-    columns[column_name] = new_column(column_name, *codes)
+    columns[column_name] = createVariable(column_name, *(codes - ['ordinal', 'onset', 'offset']))
   end
 
   # Init struct to keep track of data
@@ -52,3 +60,17 @@ begin
     current_data = {}
     code_map.each_pair do |column_name, pairs|
       values = pairs.values.map{ |i| tokens[i-1] }
+      current_data[column_name] = values
+
+      # Make new cell if current data does not match previous data
+      if values != prev_data[column_name]
+        ncell = columns[column_name].make_new_cell
+        pairs.each_pair{ |c, i| ncell.change_arg(c, tokens[i-1]) }
+      end
+    end
+
+    prev_data = current_data
+  end
+
+  columns.values.each{ |x| setVariable(x) }
+end
