c203770e3f7a0b0871b3cc4869d00289cc6732a5
Shohan Hasan <shohan.hasan@nyu.edu>
2017-06-19 14:44:02 -0400

    Add Kasey's script to add time to transcription point cells' offset.

---
+++ Transcription/AddTime-Transcribe.rb
@@ -0,0 +1,5 @@
+## Add a fixed number of miliseconds to the offset time of point cells.## Parameterscolumn_name = 'transcribe'time = 500## Bodyrequire 'Datavyu_API.rb'
+begin   trans = get_column(column_name)
+   for cell in trans.cells      if cell.onset == cell.offset        cell.change_code("offset", cell.onset+time)      end   end
+set_column(trans)
+end
