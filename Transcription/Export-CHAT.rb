ee58ebd014103519d0ff532571e93b6eebde3020
Shohan Hasan <shohan.hasan.x6@gmail.com>
2017-08-24 22:05:52 -0400

    Parameterize names of some ID codes. Add transcript processing for child based on PLAY.

--- Transcription/Export-CHAT.rb
+++ Transcription/Export-CHAT.rb
@@ -7,25 +7,52 @@
 transcript_column_name = 'transcribe'
 transcript_source_code = 'source_mb'
 transcript_content_code = 'content'
+
+# Codes from ID column
+test_date_code = 'testdate' # code in id column corresponding to test date
+birth_date_code = 'birthdate' # "" birth date
+site_code = 'site' # "" site
+participant_code = 'participant' # "" participant
+baby_language1_code = 'babylanguage1'
+baby_language2_code = 'babylanguage2'
+mom_language1_code = 'momlanguage1'
+mom_language2_code = 'momlanguage2'
+
 source_map = { # mapping from transcript_source codes to 3-letter speaker ids
 	'm'	=>	{
 		:id => 'MOT',
 		:name => nil,
 		:role => 'Mother'
+		# :site => lambda{ get_column('id').cells.first.site },
+		# :participant => lambda{ get_column('id').cells.first.participant }
+		# :language1 => lambda{ get_column('id').cells.first.momlanguage1 },
+		# :language2 => lambda{ get_column('id').cells.first.momlanguage2 }
 	},
 	'b'	=>	{
 		:id => 'CHI',
 		:name => nil,
 		:role => 'Child'
+		# :birthdate => lambda{ get_column('id').cells.first.birthdate }
+		# :language1 => lambda{ get_column('id').cells.first.babylanguage1 }
+		# :language2 => lambda{ get_column('id').cells.first.babylanguage2 }
 	}
 }
 
 substitutions = {
-	'[grunt]' => '&=grunts',
-	'[babble]' => '&=babbles',
-	'[cry]' => '&=cries'
+	# /^b$/ => '&=babbles', # these are handled manually now
+	# /^c$/ => '&=vocalizes'
 }
 
+## Body
+require 'Datavyu_API.rb'
+require 'date'
+
+java_import javax::swing::JFileChooser
+java_import javax::swing::filechooser::FileNameExtensionFilter
+
+# Globals
+utf_marker = '@UTF8'
+time_marker = "\x15" # hex code before time marker
 language_table = {
 	'Afrikaans' => 'af',
 	'Arabic' => 'ar',
@@ -62,14 +89,6 @@ language_table = {
 	'Vietnamese' => 'vi',
 	'Welsh' => 'cy'
 }
-## Body
-require 'Datavyu_API.rb'
-require 'date'
-java_import javax::swing::JFileChooser
-java_import javax::swing::filechooser::FileNameExtensionFilter
-
-utf_marker = '@UTF8'
-time_marker = "\x15" # hex code before time marker
 
 output = []
 output << utf_marker
@@ -83,8 +102,8 @@ output << header_participants
 
 # Compute age of infant from test date and birth date.
 id_cell = get_column('id').cells.first
-bd = Date.parse(id_cell.birthdate)
-td = Date.parse(id_cell.testdate)
+bd = Date.parse(id_cell.get_code(birth_date_code))
+td = Date.parse(id_cell.get_code(test_date_code))
 age = td - bd
 yrs = age/365.25
 yr = yrs.floor # years
@@ -92,18 +111,18 @@ mos = (yrs-yr) * 12
 mo = mos.to_f.round(2) # months
 
 # Get primary, secondary language(s) for child and mom
-bl = [id_cell.babylanguage1, id_cell.babylanguage2].reject{ |x| x == '.' }
+bl = [id_cell.get_code(baby_language1_code), id_cell.get_code(baby_language2_code)].reject{ |x| x == '.' }
 bls = (bl.empty?)? '' : bl.join(',')
 language_table.each_pair{ |k, v| bls.gsub!(/#{k}/i, v) }
 
-ml = [id_cell.momlanguage1, id_cell.momlanguage2].reject{ |x| x == '.' }
+ml = [id_cell.get_code(mom_language1_code), id_cell.get_code(mom_language2_code)].reject{ |x| x == '.' }
 mls = (ml.empty?)? '' : ml.join(',')
 language_table.each_pair{ |k, v| mls.gsub!(/#{k}/i, v) }
 
 # Add id headers for mom and baby.
 puts "Adding ID headers..."
-header_mom = "@ID:\t#{bls}|PLAY#{id_cell.site}|MOT||||#{id_cell.participant}|Mother|||"
-header_child = "@ID:\t#{mls}|PLAY#{id_cell.site}|CHI|#{yr};#{mo}|||#{id_cell.participant}|Child|||"
+header_mom = "@ID:\t#{bls}|PLAY#{id_cell.get_code(site_code)}|MOT||||#{id_cell.get_code(participant_code)}|Mother|||"
+header_child = "@ID:\t#{mls}|PLAY#{id_cell.get_code(site_code)}|CHI|#{yr};#{mo}|||#{id_cell.get_code(participant_code)}|Child|||"
 output << header_mom << header_child
 
 # Iterate over transcript cells
@@ -113,14 +132,30 @@ transcript_col.cells.each do |tc|
 	speaker = tc.get_code(transcript_source_code)
 	transcript = tc.get_code(transcript_content_code).strip
 
+	# Replace using substitutions hash
+	transcript.gsub!(Regexp.union(substitutions.keys), substitutions) unless substitutions.empty?
+
+	# For infant/child, lookup coding in babyutterancetype column for babbles and cries
+	# and substitute appropriately.
+	if(transcript == 'c')
+		utterance_cell = get_column('babyutterancetype').cells.find{ |x| x.onset == tc.onset }
+		case(utterance_cell.crygrunt_cg)
+		when('c')
+			transcript = '&=cry'
+		when('g')
+			transcript = '&=grunt'
+		else
+			raise "Unhandled cry utterance type."
+		end
+	elsif(transcript == 'b')
+		transcript = '&=babble'
+	end
+	
 	# Append a period to transcript unless it has ending.
 	transcript += ' .' unless %w(. ? !).any?{ |x| transcript.end_with?(x) }
 
 	# Make sure there is a space before punctuations.
 	transcript.gsub!(/([^ ])([,.?!])/, '\\1 \\2')
-
-	# Replace using substitutions hash
-	transcript.gsub!(Regexp.union(substitutions.keys), substitutions)
 
 	line = "*#{source_map[speaker][:id]}:\t#{transcript} #{time_marker}#{tc.onset}_#{tc.offset}#{time_marker}"
 	output << line
