b66ed44b2197d6d7a2b9568e6fc609716b437277
Joey Perricone <49670560+jperricone@users.noreply.github.com>
2020-09-23 12:46:26 -0400

    Insert cells into new column time-locked to transcription cells based on content matching (English/Spanish)

---
+++ Insert/insert_english_mathwords.rb
@@ -0,0 +1,99 @@
+## Parameters
+#RL_36_B_ML2 copy.opf
+# file with list of words/phrases of interest in english
+content_file = '~/Desktop/English Word List.csv'
+# allowed punctuation at end of word
+punctuation_list = %w[? ! , .]
+
+## Body
+require 'Datavyu_API.rb'
+require 'csv'
+
+CSV.open(File.expand_path(content_file))
+
+# get the transcribe column
+transcribe = get_column('transcribe')
+
+# read CSV file as array of words
+# take the first element of every row
+key_content = CSV.read(File.expand_path(content_file)).map { |r| r[0] }
+# strange bug where sometimes a space is inserted before the first character of
+# the first entry upon reading CSV file. do this for now:
+key_content[0] = 'one'
+
+# get list of all phrase lengths in list
+content_length = key_content.map { |c| c.split(' ').length }.uniq
+
+# initialize a new column to store content matches in
+english_mathwords = new_column('english_mathwords','words')
+
+# if english_categories column does not exist, then make it
+# otherwise fetch the existing column
+# this way we don't overwrite manual coding done in categories column
+unless get_column_list.include?('english_categories')
+  # intialize a new column to store word categories in as well
+  english_categories = new_column('english_categories',%w[number shapes mag_comp
+    loc_dir or deictics feat_prop pattern time false generic])
+  else
+    english_categories = get_column('english_categories')
+end
+
+# loop through cells in transcribe column
+transcribe.cells.each do |tcell|
+  # get the content/transcription for current cell
+  content = tcell.content
+
+  # split content into words
+  content_words = content.split(' ')
+  # strip words of punctuation as last character
+  content_words = content_words.map { |cw|
+    (punctuation_list.include?(cw[-1]) ? cw[0..-2] : cw) }
+  # strip words of apostrophe s too
+  content_words = content_words.map { |cw|
+    (cw[-2..-1] == "'s" ? cw[0..-3] : cw) }
+
+  # get initial list of any words that are contained in key content list
+  # include repeats!
+  key_words = content_words.select{ |cw| key_content.include?(cw) }
+
+  # now construct all sequential phrases in the list and check for matches
+  content_length.each do |x|
+    ix = 0
+    while ix < content_words.length - x
+      # construct phrase of length x
+      content_phrase = content_words[ix..ix+x].join(' ')
+      # if that phrase has an exatch match, add it to the list
+      if key_content.include?(content_phrase)
+        key_words << content_phrase
+      end
+      ix += 1
+    end
+  end
+
+  unless key_words.empty?
+    words_code = key_words.join(', ')
+    # make a new cell with same onset and offset as transcribe cell
+    # also populate the words code
+    ncell = english_mathwords.new_cell()
+    ncell.onset = tcell.onset
+    ncell.offset = tcell.offset
+    ncell.words = words_code
+
+    # see if there is already an existing corresponding category cell
+    # in which case we do not want to overwrite it
+    category_cell = english_categories.cells.select{ |c| c.onset == tcell.onset }
+    # only make new cell if it's empty
+    if category_cell.empty?
+      # make corresponding cell for categories as well
+      # for now, leave its codes blank
+      ncell = english_categories.new_cell()
+      ncell.onset = tcell.onset
+      ncell.offset = tcell.offset
+    end
+  end
+
+end
+
+# reflect these changes in the DV spreadsheet
+set_column('english_mathwords', english_mathwords)
+set_column('english_categories', english_categories)
---
+++ Insert/insert_spanish_verbtypes.rb
@@ -0,0 +1,84 @@
+## Parameters
+# folder with csv files of lists of verbs in english and spanish
+content_folder = '~/Desktop/PathManner/'
+# name of column with transcriptions
+transcription_col_name = 'Speech'
+# name of code in column with transcriptions that designates content
+content_code_name = 'transcription'
+# name of code in column with transcriptions that designates source
+source_code_name = 'speaker'
+
+# allowed punctuation at end of word
+punctuation_list = %w[? ! , .]
+
+## Body
+require 'Datavyu_API.rb'
+require 'csv'
+
+content_path = File.expand_path(content_folder)
+spanish_files = Dir.chdir(content_path) { Dir.glob('*Spanish*') }
+path_file = spanish_files.select{ |f| f.include?('Path') }.first
+manner_file = spanish_files.select{ |f| f.include?('Manner') }.first
+
+# read CSV file as array of words
+path_content = CSV.read(File.join(content_path, path_file),
+  encoding: 'iso-8859-1:utf-8')
+path_content.flatten!
+# get rid of first entry (header)
+path_content.delete_at(0)
+# read CSV file as array of words
+manner_content = CSV.read(File.join(content_path, manner_file),
+  encoding: 'iso-8859-1:utf-8')
+manner_content.flatten!
+# get rid of first entry (header)
+manner_content.delete_at(0)
+
+# fetch column with transcriptions
+transcribe = get_column(transcription_col_name)
+
+# intiialize columns in which to store verb type matches from transcriptions
+path_verbs = new_column('pathverbs', %w[source verbs])
+manner_verbs = new_column('mannerverbs', %w[source verbs])
+
+transcribe.cells.each do |c|
+
+  # get the content/transcription for current cell
+  content = c.get_code(content_code_name)
+
+  # split content into words
+  content_words = content.split(' ')
+  # strip words of punctuation as last character
+  content_words = content_words.map { |cw|
+    (punctuation_list.include?(cw[-1]) ? cw[0..-2] : cw) }
+  # strip words of apostrophe s too
+  content_words = content_words.map { |cw|
+    (cw[-2..-1] == "'s" ? cw[0..-3] : cw) }
+
+  # get initial list of any matches from path/manner verb lists
+  # include repeats!
+  path_matches = content_words.select{ |cw| path_content.include?(cw) }
+  manner_matches = content_words.select{ |cw| manner_content.include?(cw) }
+
+  # populate their columns
+  unless path_matches.empty?
+    ncell = path_verbs.new_cell()
+    ncell.onset = c.onset
+    ncell.offset = c.offset
+    ncell.source = c.get_code(source_code_name)
+    # put commas between all verbs in list as argument
+    ncell.verbs = path_matches.join(', ')
+  end
+
+  unless manner_matches.empty?
+    ncell = manner_verbs.new_cell()
+    ncell.onset = c.onset
+    ncell.offset = c.offset
+    ncell.source = c.get_code(source_code_name)
+    # put commas between all verbs in list as argument
+    ncell.verbs = manner_matches.join(', ')
+  end
+
+end
+
+set_column('pathverbs', path_verbs)
+set_column('mannerverbs', manner_verbs)
