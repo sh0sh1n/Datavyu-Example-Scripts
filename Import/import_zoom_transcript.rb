3d2401d6556859eb174b1130c2d5ce1a2a1eb73b
Joey Perricone <49670560+jperricone@users.noreply.github.com>
2020-10-30 12:25:19 -0400

    make import_zoom_transcript more robust

--- Import/import_zoom_transcript.rb
+++ Import/import_zoom_transcript.rb
@@ -1,13 +1,25 @@
 ## Before running this script: put all your zoom exported transcript .vtt files
-## into a folder on your desktop called zoom_transcript. Running it will insert
-## and populate a transcribe column in your currently open spreadsheet
+## and/or .txt files into a folder on your desktop called zoom_transcript.
+## Running this script will insert and populate a transcribe column in your
+## currently open datavyu spreadsheet.
 
 ## This script imports time-stamped transcriptions exported from zoom video chats
-## in the form of .vtt files into the currently open datavyu spreadsheet.
+## in the form of .vtt files (or .txt files) into the currently open datavyu
+## spreadsheet.
 ## It will create a new column called transcript with codes <source> and
-## <content> with a cell for each transcription
+## <content> with a cell for each transcription.
 
 ## Parameters
+## IMPORTANT parameter! Look in your .vtt or .txt file produced by zoom
+## and see which letters begin each speaker/source line
+## e.g.
+## 1
+## 00:00:02.460 --> 00:00:03.030
+## Bob
+## If there is one speaker named Bob, make this parameter ['B']
+## If there are two speakers Alice and Bob, make this parameter ['A', 'B']
+## etc.
+speaker_first_letters = ['B','P']
 # folder that contains the zoom .vtt files with transcripts
 input_folder = '~/Desktop/zoom_transcript'
 # name of column in which to import transcription data
@@ -29,57 +41,67 @@ source_list = []
 content_list = []
 # get a list of all .vtt zoom transcript files
 infiles = Dir.chdir(input_path){ Dir.glob('*.vtt') }
-# now iterate over each .vtt file and populate arrays with onset, offset, source,
-# and content
+# add .txt files too
+infiles << Dir.chdir(input_path){ Dir.glob('*.txt') }
+infiles.flatten!
+
+# now iterate over each .vtt/.txt file and populate arrays with onset, offset,
+# source, and content
 infiles.each do |infile|
   # open the current file
   file = File.open(File.join(input_path,infile))
-  # iterate over each line in the file
-  file.readlines.each do |line|
-    # skip the line if it's WEBVTT (first line of file) or a return
-    if line[0]=="W" || line[0]=="\r"
-      next
-    else
-      if line[2]==':'
-        # get characters denoting onset HH:MM:SS.mmm
-        zoom_onset = line[0..11]
-        # get characters denoting onset HH:MM:SS.mmm
-        zoom_offset = line[17..-3]
-        # parse hours, minutes, seconds, and milliseconds
-        zoom_onset_HH = zoom_onset.split(':')[0]
-        zoom_onset_MM = zoom_onset.split(':')[1]
-        zoom_onset_SS = zoom_onset.split(':')[2].split('.')[0]
-        zoom_onset_mmm = zoom_onset.split(':')[2].split('.')[1]
-        # same for offset
-        zoom_offset_HH = zoom_offset.split(':')[0]
-        zoom_offset_MM = zoom_offset.split(':')[1]
-        zoom_offset_SS = zoom_offset.split(':')[2].split('.')[0]
-        zoom_offset_mmm = zoom_offset.split(':')[2].split('.')[1]
-        # convert to absolute milliseconds for datavyu times
-        dv_onset = zoom_onset_HH.to_i*60*60*1000 + zoom_onset_MM.to_i*60*1000 +
-          zoom_onset_SS.to_i*1000 + zoom_onset_mmm.to_i
-        dv_offset = zoom_offset_HH.to_i*60*60*1000 + zoom_offset_MM.to_i*60*1000 +
-          zoom_offset_SS.to_i*1000 + zoom_offset_mmm.to_i
-        onset_list << dv_onset
-        offset_list << dv_offset
-      # see if it's a line with the source of the speaker
-      elsif line[0]=='P'
-        # seems that some lines don't have a source preceeding the colon
-        if line.split(':').length == 1
-          source = ''
-          content_line = line.split(':')[0]
-          content = content_line[0..-3]
-        else
-          source = line.split(':')[0]
-          content_line = line.split(':')[1]
-          content = content_line[1..-3]
-        end
-        source_list << source
-        content_list << content
-      end
+  header = file.readline()
+  # transcriptions are done in batches of 3 with one empty line between
+  while true
+    # empty line
+    empty_line = file.readline()
+    # break out of loop if you've reached the end of the file
+    if file.eof?
+      break
     end
+    # contains ordinal
+    ordinal_line = file.readline()
+    # contains timestamps
+    timestamp_line = file.readline()
+    # contains source and content
+    source_content_line = file.readline()
+
+    # get characters denoting onset HH:MM:SS.mmm
+    zoom_onset = timestamp_line[0..11]
+    # get characters denoting onset HH:MM:SS.mmm
+    zoom_offset = timestamp_line[17..-3]
+    # parse hours, minutes, seconds, and milliseconds
+    zoom_onset_HH = zoom_onset.split(':')[0]
+    zoom_onset_MM = zoom_onset.split(':')[1]
+    zoom_onset_SS = zoom_onset.split(':')[2].split('.')[0]
+    zoom_onset_mmm = zoom_onset.split(':')[2].split('.')[1]
+    # same for offset
+    zoom_offset_HH = zoom_offset.split(':')[0]
+    zoom_offset_MM = zoom_offset.split(':')[1]
+    zoom_offset_SS = zoom_offset.split(':')[2].split('.')[0]
+    zoom_offset_mmm = zoom_offset.split(':')[2].split('.')[1]
+    # convert to absolute milliseconds for datavyu times
+    dv_onset = zoom_onset_HH.to_i*60*60*1000 + zoom_onset_MM.to_i*60*1000 +
+      zoom_onset_SS.to_i*1000 + zoom_onset_mmm.to_i
+    dv_offset = zoom_offset_HH.to_i*60*60*1000 + zoom_offset_MM.to_i*60*1000 +
+      zoom_offset_SS.to_i*1000 + zoom_offset_mmm.to_i
+    onset_list << dv_onset
+    offset_list << dv_offset
+
+    # seems that some lines don't have a source preceeding the colon
+    if source_content_line.split(':').length == 1
+      source = ''
+      content = source_content_line.split(':')[0][0..-3]
+    else
+      source = source_content_line.split(':')[0]
+      content = source_content_line.split(':')[1][1..-3]
+    end
+    source_list << source
+    content_list << content
+
   end
 end
+
 # initialize new column with codes for transcript
 transcribe = new_column(transcript_col_name, [speaker_id_code, transcript_code])
 
@@ -93,5 +115,5 @@ onset_list.each_with_index do |x,i|
   ncell.change_code(speaker_id_code, source_list[i])
   ncell.change_code(transcript_code, content_list[i])
 end
-# reflect these changes in the datavyu spreadsheet 
+# reflect these changes in the datavyu spreadsheet
 set_column(transcript_col_name, transcribe)
